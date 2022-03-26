3437803f26c1e2e4d0f753a3c8b53e20d93f7762
SimonVail <35699287+SimonVail@users.noreply.github.com>
2022-03-26 12:07:17 -0400

    Create secrets.py

---
+++ Hydrometer/secrets.py
@@ -0,0 +1,11 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'your network',
+    'password' : 'network password',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'aio_username': 'Username',
+    'aio_password': 'Password',
+    'aio_key' : 'key'
+    }
